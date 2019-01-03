57a00b4b206215cc950d6da6ee8b55142966fcd1
Kevin Moura <kevinsmoura@hotmail.com>
2019-01-03 15:36:30 -0300

    Metodo Listar Categorias

--- src/main/java/com/kevinmoura/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/kevinmoura/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.kevinmoura.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kevinmoura.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "API REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		
+		lista.add(c1);
+		lista.add(c2);
+		
+		return lista;
 	}
 
 }
