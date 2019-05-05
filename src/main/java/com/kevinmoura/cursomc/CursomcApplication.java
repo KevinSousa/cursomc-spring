8b87cba65cede4575c7dfebf280f9a20da68b49b
KevinSousa <kevinsmoura@hotmail.com>
2019-05-05 10:46:55 -0300

    Criação de operação de Instaciação aula 15

--- src/main/java/com/kevinmoura/cursomc/CursomcApplication.java
+++ src/main/java/com/kevinmoura/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.kevinmoura.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.kevinmoura.cursomc.domain.Categoria;
+import com.kevinmoura.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
