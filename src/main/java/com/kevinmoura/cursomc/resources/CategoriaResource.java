81b21579260e0abee04bc36caa824207d032f18c
KevinSousa <kevinsmoura@hotmail.com>
2019-12-01 10:13:47 -0300

    Listando categorias com DTO

--- src/main/java/com/kevinmoura/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/kevinmoura/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.kevinmoura.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.kevinmoura.cursomc.domain.Categoria;
+import com.kevinmoura.cursomc.dto.CategoriaDTO;
 import com.kevinmoura.cursomc.services.CategoriaService;
 
 @RestController
@@ -23,10 +26,8 @@ public class CategoriaResource {
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
-
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
-
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
@@ -47,6 +48,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/com/kevinmoura/cursomc/services/CategoriaService.java
+++ src/main/java/com/kevinmoura/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.kevinmoura.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,38 +9,43 @@ import org.springframework.stereotype.Service;
 
 import com.kevinmoura.cursomc.domain.Categoria;
 import com.kevinmoura.cursomc.repositories.CategoriaRepository;
-import com.kevinmoura.cursomc.services.exceptions.ObjectNotFoundException;
 import com.kevinmoura.cursomc.services.exceptions.DataIntegrityViolationExceptionPersonalized;
+import com.kevinmoura.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + 
-				", Tipo: " + Categoria.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
-		try {			
+		try {
 			repo.deleteById(id);
-		} catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityViolationExceptionPersonalized("Não é possível remover uma categoria que possui produtos");
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationExceptionPersonalized(
+					"Não é possível remover uma categoria que possui produtos");
 		}
 	}
-	
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+
 }
