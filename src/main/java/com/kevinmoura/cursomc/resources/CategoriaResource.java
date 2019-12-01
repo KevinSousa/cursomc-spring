a25df600098db1144c5586cc4da4b342e28205e8
KevinSousa <kevinsmoura@hotmail.com>
2019-12-01 09:23:55 -0300

    Criando método update

--- src/main/java/com/kevinmoura/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/kevinmoura/cursomc/resources/CategoriaResource.java
@@ -36,5 +36,12 @@ public class CategoriaResource {
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return null;
+	}
 
 }
--- src/main/java/com/kevinmoura/cursomc/services/CategoriaService.java
+++ src/main/java/com/kevinmoura/cursomc/services/CategoriaService.java
@@ -26,4 +26,9 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
 }
